c850edb67529ff893a0c7fecb27d1d1337417aa6
eduardogomescampos1 <eduardogc0303@usp.br>
2023-06-27 20:24:47 -0300

    Versao demo do arquivo: erro de win + testbench

--- ep2/startrekassault.vhd
+++ ep2/startrekassault.vhd
@@ -39,7 +39,8 @@ begin
   preOut <= bit_vector(internal);
   parallel_out <= preOut(7 downto 0);
 end architecture;
-
+library ieee;
+use ieee.numeric_bit.all;
 -- Registrador de 8 bits capaz de subtrair a entrada de seu conteúdo. Satura-se em 0
 entity decrementerSaturated8 is
   port (
@@ -74,7 +75,8 @@ begin
   parallel_out <= "00000000" when preOut(8) = '1' else --valores negativos: saturar em 0
   				  preout(7 downto 0);
 end architecture; 
-
+library ieee;
+use ieee.numeric_bit.all;
 entity StarTrekAssaultUC is
   port (
   clock, reset, dead, gameOver, shieldCompromised, slightDamage: in bit;
@@ -85,23 +87,24 @@ entity StarTrekAssaultUC is
 architecture UC of StarTrekAssaultUC is
   -- Vamos dividir a arquitetura em estados: 1o - damage < 32,sem efeito, vida e shield max. 2o 32 < damage and shield > 128, +16, retorna c reset.
   -- 3o shield < 128, so incrementa 2, retorna c reset. W = 1 if health == 0, L = 1 if turn >= 16
-    type state_type is (IDLE ,SAFE, DANGER, COLLAPSE, GAMEOVER);
+    type state_type is (IDLE ,SAFE, DANGER, COLLAPSE, ENDGAMEOVER);
     signal present_state, next_state: state_type;
     begin
-      next_state <= SAFE when (present_state = SAFE and slightDamage = '0') else
+      next_state <= SAFE when present_state = IDLE else
+                    SAFE when (present_state = SAFE and slightDamage = '0') else
                     DANGER when (present_state = SAFE and slightDamage = '1' and shieldCompromised = '0') else
                     DANGER when (present_state = DANGER and shieldCompromised = '0') else
-                    COLLAPSE when (present_state = DANGER or present_state = SAFE and shieldCompromised = '1') else
+                    COLLAPSE when (present_state = DANGER or present_state = SAFE) and shieldCompromised = '1' else
                     COLLAPSE when (present_state = COLLAPSE and gameOver = '0') else
-                    GAMEOVER when (gameOver = '1' or dead = '1') else
-                    GAMEOVER when (present_state = GAMEOVER) else
+                    ENDGAMEOVER when (gameOver = '1' or dead = '1') else
+                    ENDGAMEOVER when (present_state = ENDGAMEOVER) else
                     SAFE;
       
-      set <= '1' when present_state = SAFE else '0';
-      ClockOrResetReaction:process(clock, reset) {    
+      set <= '1' when present_state = SAFE  else '0';
+      ClockOrResetReaction:process(clock)   
         begin
-        if (reset = 1) then
-          present_state = IDLE;
+        if (reset = '1') then
+        present_state <= IDLE;
         elsif rising_edge(clock) then 
           present_state <= next_state;
         end if;
@@ -112,35 +115,37 @@ architecture UC of StarTrekAssaultUC is
       clearSh <= '1' when present_state = IDLE else '0';
       clearCo <= '1' when present_state = IDLE else '0';
       clearLi <= '1' when present_state = IDLE else '0';
-      enSh <= '0' when (present_state = IDLE or present_state = GAMEOVER) else '1';
-      enCo <= '0' when (present_state = IDLE or present_state = GAMEOVER) else '1';
-      enLi <= '0' when (present_state = IDLE or present_state = GAMEOVER) else '1';
+      enSh <= '0' when (present_state = IDLE or present_state = ENDGAMEOVER) else '1';
+      enCo <= '0' when (present_state = IDLE or present_state = ENDGAMEOVER) else '1';
+      enLi <= '0' when (present_state = IDLE or present_state = ENDGAMEOVER) else '1';
       selRec <= '1' when (present_state = DANGER) else '0';
     end UC;
-  
-  entity counter is
+    library ieee;
+    use ieee.numeric_bit.all;
+  entity contador is
   port (
     clock, enable, reset : in bit;
     turn : out bit_vector(4 downto 0)
   );
   end entity;
-  architecture counterArch of counter is 
+  architecture counterArch of contador is 
   begin
     p0: process (clock) is
-    variable counter : unsigned (4 downto 0); -- variável
+    variable counting : unsigned (4 downto 0); -- variável
     begin
     if (reset = '1') then
-      counter := "00000"; -- valor inicial
-    elsif (rising_edge(clock) and enable = 1) then
-      counter := counter + 1;
+      counting := "00000"; -- valor inicial
+    elsif (rising_edge(clock) and enable = '1') then
+      counting := counting + 1;
     end if;
-    turn <= bit_vector(counter); -- “cast” de variable
+    turn <= bit_vector(counting); -- “cast” de variable
     end process p0;
   end architecture counterArch;
-
+  library ieee;
+  use ieee.numeric_bit.all;
   entity StarTrekAssaultFD is 
   port (
-    enSh, enLi, enCo, clearCo, clearSh, clearLi, selRec, clock, reset, ignoreDamage: in bit;
+    enSh, enLi, enCo, clearCo, clearSh, clearLi, selRec, clock, reset, ignoreDamage, set: in bit;
     slightDamage, shieldCompromised, dead, gameOver : out bit;
     damage: in bit_vector(7 downto 0); -- Entrada de dados: dano
     shield: out bit_vector(7 downto 0); -- Saída: shield atual
@@ -165,6 +170,12 @@ architecture UC of StarTrekAssaultUC is
         parallel_out: out bit_vector(7 downto 0)	-- Conteúdo do registrador: 8 bits, representando 0 a 255
       );
     end component;
+    component contador is
+      port (
+        clock, enable, reset : in bit;
+        turn : out bit_vector(4 downto 0)
+      );
+      end component;
     signal shieldBuffer, healthBuffer, healthChange, recovery : bit_vector(7 downto 0);
     signal shieldChange : bit_vector(8 downto 0);
     signal turnBuffer : bit_vector (4 downto 0);
@@ -173,21 +184,23 @@ architecture UC of StarTrekAssaultUC is
         recovery <= "00010000" when '1',
                     "00000010" when '0',
                     "00000000" when others;
-      slightDamage <= '1' when (signed(damage) > 32) else '0';
-      shieldCompromised <= '1' when (unsigned(shieldBuffer) <= 128) else '0';
-      shieldChange <=  '0' when ignoreDamage = '1' else bit_vector(signed('0' & recovery) - signed('0' & damage));
-      healthChange <=  '0' when (signed(shieldChange) + signed('0' & shieldBuffer)) > 0 else bit_vector(unsigned(damage) - unsigned(recovery) - unsigned(shieldBuffer)); 
+      slightDamage <= '1' when (signed(damage) > 31) else '0';
+      shieldCompromised <= '1' when (unsigned(shieldBuffer) < 128) else '0';
+      shieldChange <=  "000000000" when ignoreDamage = '1' else bit_vector(signed('0' & recovery) - signed('0' & damage));
+      healthChange <=  "00000000" when (signed(shieldChange) + signed('0' & shieldBuffer)) > 0 else bit_vector(unsigned(damage) - unsigned(recovery) - unsigned(shieldBuffer)); 
       shieldManipulation : adderSaturated8 port map(clock, set, clearSh, enSh, shieldChange, shieldBuffer);
       healthManipulation : decrementerSaturated8 port map(clock, set, clearLi, enLi, healthChange, healthBuffer);
       counterManipulation : contador port map (clock, enCo, clearCo, turnBuffer); 
-      dead <= '1' when healthBuffer = "00000000" else '0';
+      dead <= '1' when healthBuffer = "00000000" and turnBuffer /= "00000" else '0';
       gameOver <= '1' when turnBuffer = "10000" else '0';
       shield <= shieldBuffer;
       health <= healthBuffer;
       turn <= turnBuffer;  
   end FD;
+  library ieee;
+use ieee.numeric_bit.all;
   
-  entity StarTrekAssault is
+  entity StartTrekAssault is
     port (
     clock, reset: in bit; -- sinais de controle globais
     damage: in bit_vector(7 downto 0); -- Entrada de dados: dano
@@ -197,15 +210,33 @@ architecture UC of StarTrekAssaultUC is
     WL: out bit_vector(1 downto 0) -- Saída: vitória e/ou derrota
     );
     end entity;
-  architecture StarTrekAssaultArch of StarTrekAssault is 
-  signal clearCo, clearLi, clearSh, enCo, enSh, enLi, gameOver, slightDamage, shieldCompromised, dead, selRec, ignoreDamage, set : bit;
+  architecture StarTrekAssaultArch of StartTrekAssault is
+    component StarTrekAssaultFD is 
+    port (
+      enSh, enLi, enCo, clearCo, clearSh, clearLi, selRec, clock, reset, ignoreDamage, set: in bit;
+      slightDamage, shieldCompromised, dead, gameOver : out bit;
+      damage: in bit_vector(7 downto 0); -- Entrada de dados: dano
+      shield: out bit_vector(7 downto 0); -- Saída: shield atual
+      health: out bit_vector(7 downto 0); -- Saída: health atual
+      turn: out bit_vector(4 downto 0) -- Saída: rodada atual
+    );
+    end component;
+    component StarTrekAssaultUC is
+      port (
+      clock, reset, dead, gameOver, shieldCompromised, slightDamage: in bit;
+      enSh, enLi, enCo, clearCo, clearLi, clearSh, selRec, ignoreDamage, set: out bit;
+      WL : out bit_vector(1 downto 0)
+      );
+      end component;
+  signal clearCo, clearLi, clearSh, enCo, enSh, enLi, gameOver, slightDamage, shieldCompromised, dead, selRec, ignoreDamage, set, n_clock : bit;
   signal damageBuff, healthBuff, shieldBuff : bit_vector (7 downto 0);
   signal turnBuff : bit_vector(4 downto 0);
   signal WLbuff : bit_vector(1 downto 0);
-  begin 
+  begin
+      n_clock <= not clock;
       damageBuff <= damage;
       UnitControl: StarTrekAssaultUC port map (clock, reset, dead, gameOver, shieldCompromised, slightDamage, enSh, enLi, enCo, clearCo, clearLi, clearSh, selRec, ignoreDamage, set, WLbuff);
-      DataFlow: StarTrekAssaultFD port map(enSh, enLi, enCo, clearCo, clearSh, clearLi, selRec, clock, reset, ignoreDamage, slightDamage, shieldCompromised, dead, gameOver, damageBuff, shieldBuff, healthBuff, turnBuff);
+      DataFlow: StarTrekAssaultFD port map(enSh, enLi, enCo, clearCo, clearSh, clearLi, selRec, n_clock, reset, ignoreDamage, set, slightDamage, shieldCompromised, dead, gameOver, damageBuff, shieldBuff, healthBuff, turnBuff);
       WL <= WLBuff;
       shield <= shieldBuff;
       health <= healthBuff;
